3bc10b42656a3009022a329e51d52aa5594fb7e1
Arjunkumar A <252754335+arjunkumar24dev@users.noreply.github.com>
2026-01-04 21:44:55 +0530

    Create Day 1 animal classifier

---
+++ day 1_animal_classifier .py
@@ -0,0 +1,11 @@
+# Day 1 : Animal kingdom Classifier
+
+animal = input("Enter an animal name:").lower()
+if animal == "human" or animal == "ape":
+  print("category : Mammal")
+elif animal == "eagle" or animal == "parrot":
+  print("Category: Bird")
+elif animal == "shark" or animal == "salmon":
+  print("Category: Fish")
+else:
+  print("Category: other species")
